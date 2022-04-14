582e150f23b9954d81b5d1fca75f01af6dc9d98f
igaimerca <igaimerca@gmail.com>
2022-04-14 20:46:31 +0200

    ft: blogs tests

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const swaggerJson = require("./swagger.json");
 config({ path: "./.env" });
 
 mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-    .then(() => {
+.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+.then(() => {
         const app = express();
         app.use(express.json());
         app.use("/blogs", blogRoutes);
