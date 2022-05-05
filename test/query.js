30275ef7c8a94455b8ed0e6ee887b83bdee88a1a
igaimerca <igaimerca@gmail.com>
2022-05-05 20:40:38 +0200

    fix: post query test

--- test/query.js
+++ test/query.js
@@ -40,8 +40,6 @@ describe("Queries Endpoints", () => {
      */
     describe("POST /contact", () => {
         it("It should create a new query", (done) => {
-            const token =
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU3MGM4ODBmNzhlMWQyZjUwNDUzNzMiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NDk5NjA2MTl9.JhWPMPZbb1EF9GwU7UX5FHZ-WMTORC6BCHE6nngQGuQ";
             const query = {
                 name: "name",
                 email: "name@gmail.com",
@@ -51,7 +49,6 @@ describe("Queries Endpoints", () => {
             chai
                 .request(server)
                 .post("/contact")
-                .set({ Authorization: `Bearer ${token}` })
                 .send(query)
                 .end((err, response) => {
                     response.should.have.status(201);
